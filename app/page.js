836f4f3ee620037ad5c2fd0dbcb675feb99c1705
joaquincodigo <joaquin.codigo@gmail.com>
2024-06-11 18:24:49 -0300

    more of the general html structure

--- app/page.js
+++ app/page.js
@@ -1,13 +1,35 @@
 export default function Home() {
   return (
     <>
-      <header className="bg-blue-300">Banner</header>
-      {/* <nav className="bg-gray-300">Navbar</nav> */}
-      <div className="ContentContainer flex flex-row">
-        <aside className="bg-teal-300">Left Sidebar</aside>
-        <main>
-          <div className="bg-slate-600">Filters Bar</div>
-          <table className="bg-orange-300 table-auto">
+      <header className="bg-blue-300">
+        Banner
+        <div>Filler Content</div>
+        <div>Filler Content</div>
+      </header>
+      <div className="ContentContainer flex flex-row w-full bg-violet-500">
+        <aside className="bg-teal-300 flex-1 flex flex-col">
+          <div className="bg-violet-600 mx-3 mt-3">
+            Advertisement 1 Advertisement 1 Advertisement 1 Advertisement 1
+            Advertisement 1 Advertisement 1 Advertisement 1 Advertisement 1
+            Advertisement 1
+          </div>
+          <div className="bg-violet-600 mx-3 mt-3">
+            Advertisement 2 Advertisement 2 Advertisement 2 Advertisement 2
+            Advertisement 2 Advertisement 2 Advertisement 2 Advertisement 2
+            Advertisement 2
+          </div>
+          <div className="bg-violet-600 mx-3 mt-3">
+            Advertisement 3 Advertisement 3 Advertisement 3 Advertisement 3
+            Advertisement 3 Advertisement 3 Advertisement 3 Advertisement 3
+            Advertisement 3
+          </div>
+        </aside>
+        <main className="flex-grow">
+          <div className="bg-slate-600 sticky top-0">
+            <div>FilterBar</div>
+            <div>FillerConter</div>
+          </div>
+          <table className="bg-orange-300 w-full overflow-scroll max-h-10">
             <thead>
               <tr>
                 <th>Name</th>
@@ -76,10 +98,346 @@ export default function Home() {
                 <td>x12</td>
                 <td>x7</td>
               </tr>
+              <tr>
+                <td>Talking Island</td>
+                <td>Start your adventure here</td>
+                <td>Interlude</td>
+                <td>x3</td>
+                <td>x2</td>
+                <td>x1</td>
+              </tr>
+              <tr>
+                <td>Heine</td>
+                <td>The water city awaits</td>
+                <td>Gracia Final</td>
+                <td>x7</td>
+                <td>x4</td>
+                <td>x3</td>
+              </tr>
+              <tr>
+                <td>Hunter's Village</td>
+                <td>Perfect for PvP lovers</td>
+                <td>High Five</td>
+                <td>x12</td>
+                <td>x5</td>
+                <td>x3</td>
+              </tr>
+              <tr>
+                <td>Schuttgart</td>
+                <td>Enter the snowy paradise</td>
+                <td>Freya</td>
+                <td>x18</td>
+                <td>x9</td>
+                <td>x4</td>
+              </tr>
+              <tr>
+                <td>Rune</td>
+                <td>Home of the nobles</td>
+                <td>Epilogue</td>
+                <td>x22</td>
+                <td>x11</td>
+                <td>x5</td>
+              </tr>
+              <tr>
+                <td>Goddard</td>
+                <td>Unveil the mysteries of the north</td>
+                <td>Interlude</td>
+                <td>x4</td>
+                <td>x3</td>
+                <td>x2</td>
+              </tr>
+              <tr>
+                <td>Innadril</td>
+                <td>The jewel of the lake</td>
+                <td>Gracia Final</td>
+                <td>x9</td>
+                <td>x6</td>
+                <td>x4</td>
+              </tr>
+              <tr>
+                <td>Gludin</td>
+                <td>The town of traders</td>
+                <td>High Five</td>
+                <td>x14</td>
+                <td>x7</td>
+                <td>x3</td>
+              </tr>
+              <tr>
+                <td>Florin</td>
+                <td>Explore the enchanted forest</td>
+                <td>Freya</td>
+                <td>x20</td>
+                <td>x8</td>
+                <td>x4</td>
+              </tr>
+              <tr>
+                <td>Devastated Castle</td>
+                <td>Conquer the ruins</td>
+                <td>Epilogue</td>
+                <td>x26</td>
+                <td>x13</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Wastelands</td>
+                <td>Survive the harsh lands</td>
+                <td>Interlude</td>
+                <td>x6</td>
+                <td>x4</td>
+                <td>x2</td>
+              </tr>
+              <tr>
+                <td>Elven Village</td>
+                <td>Feel the magic of the elves</td>
+                <td>Gracia Final</td>
+                <td>x8</td>
+                <td>x5</td>
+                <td>x3</td>
+              </tr>
+              <tr>
+                <td>Dark Elven Village</td>
+                <td>Embrace the shadows</td>
+                <td>High Five</td>
+                <td>x11</td>
+                <td>x6</td>
+                <td>x4</td>
+              </tr>
+              <tr>
+                <td>Orc Village</td>
+                <td>Strength and honor</td>
+                <td>Freya</td>
+                <td>x15</td>
+                <td>x8</td>
+                <td>x5</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>{" "}
+              <tr>
+                <td>Gludin</td>
+                <td>The town of traders</td>
+                <td>High Five</td>
+                <td>x14</td>
+                <td>x7</td>
+                <td>x3</td>
+              </tr>
+              <tr>
+                <td>Florin</td>
+                <td>Explore the enchanted forest</td>
+                <td>Freya</td>
+                <td>x20</td>
+                <td>x8</td>
+                <td>x4</td>
+              </tr>
+              <tr>
+                <td>Devastated Castle</td>
+                <td>Conquer the ruins</td>
+                <td>Epilogue</td>
+                <td>x26</td>
+                <td>x13</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Wastelands</td>
+                <td>Survive the harsh lands</td>
+                <td>Interlude</td>
+                <td>x6</td>
+                <td>x4</td>
+                <td>x2</td>
+              </tr>
+              <tr>
+                <td>Elven Village</td>
+                <td>Feel the magic of the elves</td>
+                <td>Gracia Final</td>
+                <td>x8</td>
+                <td>x5</td>
+                <td>x3</td>
+              </tr>
+              <tr>
+                <td>Dark Elven Village</td>
+                <td>Embrace the shadows</td>
+                <td>High Five</td>
+                <td>x11</td>
+                <td>x6</td>
+                <td>x4</td>
+              </tr>
+              <tr>
+                <td>Orc Village</td>
+                <td>Strength and honor</td>
+                <td>Freya</td>
+                <td>x15</td>
+                <td>x8</td>
+                <td>x5</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
+              <tr>
+                <td>Dwarven Village</td>
+                <td>Craft your destiny</td>
+                <td>Epilogue</td>
+                <td>x21</td>
+                <td>x11</td>
+                <td>x6</td>
+              </tr>
             </tbody>
           </table>
         </main>
-        <aside class="bg-green-300">Right Sidebar</aside>
+        <aside class="bg-green-300 flex-1">Right Sidebar</aside>
       </div>
       <footer className="bg-slate-300">Footer</footer>
     </>
