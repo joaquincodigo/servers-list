85de1e3499dc4d27bc64f20713a9d542c09f531e
joaquincodigo <joaquin.codigo@gmail.com>
2024-07-24 18:24:43 -0300

    pagination component

---
+++ components/PaginationControl.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const PaginationControl = ({ servers }) => {
+  const [linesPerPage, setLinesPerPage] = useState(15);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagesList, setPagesList] = useState([]);
+
+  useEffect(() => {
+    // First, we ensure that the amount of pages needed is an integer
+    const totalPages = Math.ceil(servers.length / linesPerPage);
+
+    if (totalPages <= 10) {
+      // Then, whe create an array with thos page. Example: pages = 3, array = [1,2,3]
+      setPagesList(Array.from({ length: 10 }, (_, i) => i + 1));
+    }
+  }, [servers, linesPerPage]);
+
+  return (
+    <div className="TableFooter flex flex-col items-center bg-slate-300">
+      <div>
+        Page {currentPage} of{" [ "}
+        {pagesList.map((element, index) => (
+          <span key={index}>{element} </span> // Corrected JSX
+        ))}
+        {" ]"}
+      </div>
+      <div>There are {servers.length} servers under the current filters</div>
+      <label htmlFor="lines">Lines per Page</label>
+      <select
+        name="lines"
+        id="lines"
+        onChange={(e) => setLinesPerPage(parseInt(e.target.value))}
+      >
+        <option value="15">15 lines per page</option>
+        <option value="30">30 lines per page</option>
+        <option value="50">50 lines per page</option>
+      </select>
+    </div>
+  );
+};
+
+export default PaginationControl;
--- components/ServersTable.jsx
+++ components/ServersTable.jsx
@@ -1,42 +1,35 @@
 import React from "react";
+import PaginationControl from "./PaginationControl";
 
-const ServersTable = ({ servers }) => (
-  <table className="bg-orange-300 w-full">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Chronicle</th>
-        <th>EXP</th>
-        <th>ADENA</th>
-        <th>SPOIL</th>
-      </tr>
-    </thead>
-    <tbody>
-      {servers.slice(0, 15).map((server, index) => (
-        <tr key={index}>
-          <td>{server.Name}</td>
-          <td>{server.Description}</td>
-          <td>{server.Chronicle}</td>
-          <td>{server.EXP}</td>
-          <td>{server.ADENA}</td>
-          <td>{server.SPOIL}</td>
-        </tr>
-      ))}
-    </tbody>
-    <div className="table-footer flex justify-center">
-      <div className="flex flex-row">
-        <div>Page ? of ?</div>
-        <div>Total Servers: {servers.length}</div>
-        <label for="lines">Lines per page</label>
-        <select name="line" id="lines">
-          <option value="20">20 lines</option>
-          <option value="50">50 lines</option>
-          <option value="100">100 lines</option>
-        </select>
-      </div>
+const ServersTable = ({ servers }) => {
+  return (
+    <div className="ServersTable">
+      <table className="bg-orange-300 w-full">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Chronicle</th>
+            <th>EXP</th>
+            <th>ADENA</th>
+            <th>SPOIL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {servers.slice(0, 15).map((server, index) => (
+            <tr key={index}>
+              <td>{server.Name}</td>
+              <td>{server.Description}</td>
+              <td>{server.Chronicle}</td>
+              <td>{server.EXP}</td>
+              <td>{server.ADENA}</td>
+              <td>{server.SPOIL}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <PaginationControl servers={servers} />
     </div>
-  </table>
-);
-
+  );
+};
 export default ServersTable;
